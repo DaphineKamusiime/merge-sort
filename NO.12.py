8192c914b6b71f1a6c60a1817e0813205a1f31ec
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-11-11 01:44:48 -0800

    Add files via upload

---
+++ NO.12.py
@@ -0,0 +1,34 @@
+def merge_sort(arr):
+    while len(arr)<1:
+        mid=int(len(arr)/2)
+        L=arr[:mid]
+        R=arr[mid:]
+        i=j=k=0
+        while i<len(L) and j<len(R):
+            if L[i]<=R[j]:
+                arr[k]=L[i]
+                i+=1
+            else:
+                arr[k]=R[j]
+                j+=1
+        k+=1
+        while i<len(L):
+            arr[k]=L[i]
+            i+=1
+            k+=1
+        while j<len(R):
+            arr[k]=R[j]
+            j+=1
+            k+=1
+            
+def print_list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+arr=["Solomon", "Daphine","Charles","Tracy","Jasper","Destiny","Nabil"]
+if __name__=='__main__':
+    print_list(arr)
+    merge_sort(arr)
+    print_list(arr)
+            
